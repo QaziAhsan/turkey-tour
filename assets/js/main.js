3fa57f9e1a70fc552160c101b9548dc0d16b3026
QaziAhsan <iamahsan96@gmail.com>
2022-03-14 14:01:04 +0500

    All

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+AOS.init();
+
+$('.owl-carousel-basic').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+$('.pd-slider').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:true,
+    dots:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        500:{
+            items:2,
+            nav:true
+        },
+        768:{
+            items:3,
+            nav:false
+        },
+        1200:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+$('.owl-carousel-testimonials').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:true,
+    dots:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        }
+    }
+})
